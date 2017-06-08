8821eb905b934c978b334362c3f9e074f659e949
Aron Heck <s67588@beuth-hochschule.de>
2017-06-08 18:01:33 +0200

    added python version of staircase numbers with nicer comments

--- applications_with_staircase_sequence/Python3/minimal_example_of_staircase_number_generation.py
+++ applications_with_staircase_sequence/Python3/minimal_example_of_staircase_number_generation.py
@@ -1,9 +1,28 @@
+"""
+   :platform: Unix, Windows
+   :synopsis: This is a minimal examples of the provided class StaircaseGenerator
+
+.. moduleauthor:: Aron Heck
+
+"""
+
+
 from codes_for_the_sequence.Python3.staircase_number_generator import StaircaseGenerator
 
-if __name__ == "__main__":
+def main():
+    """
+    Asks for lower and upper bound then print staircase numbers and reset them and print them again
+    """
     lower_bound = int(input("Enter lower bound of number range:"))
     upper_bound = int(input("Enter upper bound of number range:"))
 
     staircase = StaircaseGenerator(lower_bound, upper_bound)
     staircase.generate_staircase_from_lower_to_upper_bound()
     staircase.print_generated_numbers()
+    staircase.reset_bounds(upper_bound - lower_bound, upper_bound + lower_bound)
+    staircase.generate_staircase_from_lower_to_upper_bound()
+    staircase.print_generated_numbers()
+
+if __name__ == "__main__":
+    main()
+
--- codes_for_the_sequence/Python3/staircase_number_generator.py
+++ codes_for_the_sequence/Python3/staircase_number_generator.py
@@ -5,7 +5,6 @@
 
 .. moduleauthor:: Aron Heck
 
-
 """
 
 
@@ -28,12 +27,20 @@ class StaircaseGenerator(object):
         self.__upper_bound = upper_bound
 
     def generate_staircase_from_lower_to_upper_bound(self):
+        """
+        Call the methods needed to generate the staircase numbers and fill the staircase number list
+        """
         for i in range(self.__lower_bound, self.__upper_bound):
             if StaircaseGenerator.__check_single_number(i):
                 self.__generated_numbers.append(i)
 
     @staticmethod
     def __check_single_number(number_to_check):
+        """
+        Check a single number whether it is a staircase number or not
+        :param number_to_check: the number that is to be checked
+        :return: True if number is staircase number, False if it is not
+        """
         number_of_digits = len(str(abs(number_to_check)))
         first_digit = int(str(abs(number_to_check))[:1])
         last_digit = int(str(abs(number_to_check))[number_of_digits-1: number_of_digits])
@@ -41,11 +48,20 @@ class StaircaseGenerator(object):
         return (((first_digit * first_digit) + (last_digit * last_digit)) == all_other_digits)
 
     def reset_bounds(self, lower_bound: int, upper_bound: int):
+        """
+        Reset all class attributed to new bounds
+        :param lower_bound: new lower bound
+        :param upper_bound: new upper bound
+        """
         self.__lower_bound = lower_bound
         self.__upper_bound = upper_bound
         self.__generated_numbers = []
 
     def print_generated_numbers(self):
+        """
+        Print numbers nicely formated with additional information
+        """
+        print("Staircase numbers in range", self.__lower_bound, "to", self.__upper_bound)
         for i in range(len(self.__generated_numbers)):
-            print(i+1, " ---> ", self.__generated_numbers[i])
-        print("Total number of staircase numbers: ", i+1)
+            print(i+1, "--->", self.__generated_numbers[i])
+        print("Total number of staircase numbers:", i+1)
