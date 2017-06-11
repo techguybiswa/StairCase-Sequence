2ca20e9dddd3af3d1395ffc14faec0fb28fca75b
Aron Heck <s67588@beuth-hochschule.de>
2017-06-11 20:14:09 +0200

    edited range restrictions and made a hotfix for number generation

--- codes_for_the_sequence/Python3/minimal_example_of_staircase_number_generation.py
+++ codes_for_the_sequence/Python3/minimal_example_of_staircase_number_generation.py
@@ -13,15 +13,13 @@ def main():
     """
     Asks for lower and upper bound then print staircase numbers and reset them and print them again
     """
-    lower_bound = int(input("Enter lower bound of number range:"))
-    upper_bound = int(input("Enter upper bound of number range:"))
+    lower_bound = int(input("Enter lower bound of number range as integer decimal:"))
+    upper_bound = int(input("Enter upper bound of number range as integer decimal:"))
 
     staircase = StaircaseGenerator(lower_bound, upper_bound)
     staircase.generate_staircase_from_lower_to_upper_bound()
     staircase.print_generated_numbers()
-    staircase.reset_bounds(upper_bound - lower_bound, upper_bound + lower_bound)
-    staircase.generate_staircase_from_lower_to_upper_bound()
-    staircase.print_generated_numbers()
+
 
 if __name__ == "__main__":
     main()
